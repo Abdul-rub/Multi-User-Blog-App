1d49eed1e5663e7bd7924a153f978708640b663a
Abdul Rub <arub0419@gmail.com>
2022-12-31 19:15:58 +0530

    login-auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,29 @@
-
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Login from "./components/Login";
+import Blogs from "./components/Blogs";
+import UserBlogs from "./components/UserBlogs";
+import BlogDetails from "./components/BlogDetails";
+import AddBlog from "./components/AddBlog";
+import { useSelector } from "react-redux";
 
 function App() {
+  const isLoggedIn = useSelector(state=>state.isLoggedIn)
+  console.log(isLoggedIn)
   return (
-    <div className="App">
-   <h1>Hiii</h1>
-    </div>
+    <>
+      <header>
+        <Header />
+      </header>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/myblogs" element={<UserBlogs />} />
+        <Route path="/myblogs/:id" element={<BlogDetails />} />
+        <Route path="/blogs/add" element={<AddBlog />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Box,
+  Button,
+  Tabs,
+  Tab,
+} from "@mui/material";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+const Header = () => {
+  const isLoggedIn = useSelector((state) => state.isLoggedIn);
+  const [value, setValue] = useState();
+  return (
+    <AppBar
+      position="sticky"
+      sx={{
+        background:
+          "linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%)",
+      }}
+    >
+      <Toolbar>
+        <Typography variant="h4">BlogApp</Typography>
+        {isLoggedIn && (
+          <Box display="flex" marginLeft={"auto"} marginRight={"auto"}>
+            <Tabs
+              textColor="inherit"
+              value={value}
+              onChange={(e, val) => setValue(val)}
+            >
+              <Tab LinkComponent={Link} to="/blogs" label="All Blogs" />
+              <Tab LinkComponent={Link} to="/myblogs" label="My Blogs" />
+            </Tabs>
+          </Box>
+        )}
+        <Box display="flex" marginLeft="auto">
+          { !isLoggedIn && <> <Button
+            LinkComponent={Link}
+            to="/login"
+            variant="contained"
+            sx={{ margin: 1, borderRadius: 10 }}
+          >
+            Login
+          </Button>
+          <Button
+            LinkComponent={Link}
+            to="/login"
+            variant="contained"
+            sx={{ margin: 1, borderRadius: 10 }}
+          >
+            SignUp
+          </Button> </>}
+          {isLoggedIn && (
+            <Button
+              LinkComponent={Link}
+              to="/login"
+              variant="contained"
+              sx={{ margin: 1, borderRadius: 10 }}
+            >
+              LogOut
+            </Button>
+          )}
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
