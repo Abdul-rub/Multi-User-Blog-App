5165f76e5458c92cf44eef779faa5c0bec79050e
Abdul Rub <arub0419@gmail.com>
2023-01-01 01:01:22 +0530

    blog-card

--- client/src/components/Blogs.jsx
+++ client/src/components/Blogs.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios'
+import { useState } from 'react'
+import BlogCard from './BlogCard'
+
 
 const Blogs = () => {
+  const [blog,setBlog]= useState()
+
+
+  const getAllBlogs = ()=>{
+    axios.get(`http://localhost:8080/blog`)
+    .then((res)=>setBlog(res.data))
+    .catch((err)=>console.log(err))
+  }
+
+
+
+
+useEffect(()=>{
+getAllBlogs()
+},[])
+console.log(blog)
+
   return (
-    <div>Blogs</div>
+    <div>
+     {blog && blog.map((el,i)=>{
+      return(
+        <BlogCard />
+      )
+     }) }
+    </div>
   )
 }
 
